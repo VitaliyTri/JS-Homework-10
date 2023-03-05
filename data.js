23fffdb22da0fe0ad873d968a20900cbfa677c36
VitaliyTri <111489824+VitaliyTri@users.noreply.github.com>
2023-03-05 11:15:30 +0700

    safe

---
+++ data.js
@@ -0,0 +1,57 @@
+const jsonData = `
+[
+    {
+        "id": 573,
+        "name": "Snake Hitler",
+        "status": "Dead",
+        "species": "Animal",
+        "type": "Snake",
+        "gender": "Male",
+        "image": "https://rickandmortyapi.com/api/character/avatar/573.jpeg"
+    },
+    {
+        "id": 524,
+        "name": "Mr. Meeseeks",
+        "status": "unknown",
+        "species": "Humanoid",
+        "type": "Meeseeks",
+        "gender": "Male",
+        "image": "https://rickandmortyapi.com/api/character/avatar/524.jpeg"
+    },
+    {
+        "id": 216,
+        "name": "MC Haps",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
+        "image": "https://rickandmortyapi.com/api/character/avatar/216.jpeg"
+    },
+    {
+        "id": 621,
+        "name": "Ruth Bader Ginsburg",
+        "status": "Alive",
+        "species": "Human",
+        "type": "Soulless Puppet",
+        "gender": "Female",
+        "image": "https://rickandmortyapi.com/api/character/avatar/621.jpeg"
+    },
+    {
+        "id": 640,
+        "name": "Angry Glorzo",
+        "status": "Dead",
+        "species": "Alien",
+        "type": "Glorzo",
+        "gender": "Male",
+        "image": "https://rickandmortyapi.com/api/character/avatar/640.jpeg"
+    },
+    {
+        "id": 750,
+        "name": "Mousetrap Nipples",
+        "status": "unknown",
+        "species": "Mythological Creature",
+        "type": "Demon",
+        "gender": "Male",
+        "image": "https://rickandmortyapi.com/api/character/avatar/750.jpeg"
+    }
+]`
